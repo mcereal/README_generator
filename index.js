819dc492c0af8d6f652b2b1c5a768c27b0b583d7
mcereal <mpbcurtis@gmail.com>
2020-10-28 09:54:07 -0400

    questions for user prompt created

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
 
 inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your user name?",
+      message: "What is your Github username?",
       name: "username",
     },
     {
-      type: "password",
-      message: "What is your password?",
-      name: "password",
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
     },
     {
-      type: "password",
-      message: "Re-enter password to confirm:",
-      name: "confirm",
+      type: "input",
+      message: "What is your project's name?",
+      name: "projectName",
+    },
+    {
+      type: "input",
+      message: "Please write a brief description of your project",
+      name: "projectDetails",
+    },
+    {
+      type: "input",
+      message: "What kind of license should your project have?",
+      name: "license",
+    },
+    {
+      type: "input",
+      message: "What command should be run to install dependencies?",
+      name: "dependencies",
+    },
+    {
+      type: "input",
+      message: "What command should be run to run tests?",
+      name: "tests",
+    },
+    {
+      type: "input",
+      message: "What does the user need to know about using the repo?",
+      name: "userRepoKnowledge",
+    },
+    {
+      type: "input",
+      message:
+        "What does the user need to know about contributing to the repo?",
+      name: "contributions",
     },
   ])
   .then(function (response) {
